218373592814d69b46d08a42f0316a59ac436063
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-03-23 01:04:38 +0900

    整体認証アプリ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.biometricauth_sample"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/biometricauth_sample/MainActivity.kt
+++ app/src/main/java/com/example/biometricauth_sample/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.example.biometricauth_sample
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.biometricauth_sample.BiometricPromptManager.*
 import com.example.biometricauth_sample.ui.theme.BiometricAuth_sampleTheme
 
 class MainActivity : AppCompatActivity() {
+
+    private val promptManager by lazy {
+        BiometricPromptManager(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +41,67 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val biometricResult by promptManager.promptResult.collectAsState(
+                        initial = null
+                    )
+                    val enrollLauncher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.StartActivityForResult(),
+                        onResult = {
+                            println("Activity Result: $it")
+                        }
+                    )
+                    LaunchedEffect(biometricResult) {
+                        if(biometricResult is BiometricResult.AuthenticationNotSet) {
+                            if(Build.VERSION.SDK_INT >= 30) {
+                                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                                    putExtra(
+                                        Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL
+                                    )
+                                 }
+                                enrollLauncher.launch(enrollIntent)
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick = {
+                            promptManager.showBiometricPrompt(
+                                title = "sample prompt",
+                                description = "Sample prompt description"
+                            )
+                        }) {
+                            Text(text = "認証")
+                        }
+                        biometricResult?.let { result ->
+                            Text(
+                                text = when(result) {
+                                    is BiometricResult.AuthenticationError -> {
+                                        result.error
+                                    }
+                                    BiometricResult.AuthenticationFailed -> {
+                                        "認証に失敗しました"
+                                    }
+                                    BiometricResult.AuthenticationNotSet -> {
+                                        "認証設定がされていません"
+                                    }
+                                    BiometricResult.AuthenticationSuccess -> {
+                                        "認証に成功しました"
+                                    }
+                                    BiometricResult.FeatureUnavailable -> {
+                                        "認証が利用できません"
+                                    }
+                                    BiometricResult.HardwareUnavailable -> {
+                                        "ハードウェアが利用できません"
+                                    }
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
